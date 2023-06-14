075e5eb553903c06b3819eec6789ed589a3e3277
Taek Seung Lee <dlxortmd987@gmail.com>
2023-06-15 01:47:44 +0900

    Feat: 사업자 등록 로직 추가

--- reservation_service/src/main/java/com/numble/reservation_service/domain/seller/SellerService.java
+++ reservation_service/src/main/java/com/numble/reservation_service/domain/seller/SellerService.java
@@ -1,8 +1,10 @@
 package com.numble.reservation_service.domain.seller;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class SellerService {
 
 	private final SellerRepository sellerRepository;
